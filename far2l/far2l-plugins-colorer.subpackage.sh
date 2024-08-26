40faedba9eff72eb9d7c86ad00bc26a659f4253a
spvkgn <spvkgn@users.noreply.github.com>
2024-08-26 09:01:23 +0500

    Colorer depends on libxml2

--- far2l/far2l-plugins-colorer.subpackage.sh
+++ far2l/far2l-plugins-colorer.subpackage.sh
@@ -1,5 +1,5 @@
 TERMUX_SUBPKG_DESCRIPTION="FAR2L colorer plugin"
-TERMUX_SUBPKG_DEPENDS="libxerces-c"
+TERMUX_SUBPKG_DEPENDS="libxml2"
 TERMUX_SUBPKG_INCLUDE="
 lib/far2l/Plugins/colorer
 share/far2l/Plugins/colorer
